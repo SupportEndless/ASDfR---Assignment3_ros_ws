025aa7950c992cebc1c95fa1a7e834b6f7a82dc0
SupportEndless <rogiermeijerman@gmail.com>
2026-04-09 14:32:25 +0200

    Added launch shell file

--- src/assignment3_2/src/XRF2_FRT_test.cpp
+++ src/assignment3_2/src/XRF2_FRT_test.cpp
@@ -5,9 +5,13 @@ XRF2_FRT_test::XRF2_FRT_test(uint write_decimator_freq, uint monitor_freq) :
     file(1,"./xrf2_logging/assignment3_2","bin"), // change template to your project name
     controller()
 {
-     printf("%s: Constructing rampio\n", __FUNCTION__);
-    // Add variables to logger to be logged, has to be done before you can log data
+    printf("%s: Constructing rampio\n", __FUNCTION__);
 
+    // Add variables to logger to be logged, has to be done before you can log data
+    logger.addVariable("channel1", id_int);
+    logger.addVariable("channel2", id_int);
+    logger.addVariable("channel3", id_int);
+    logger.addVariable("channel4", id_int);
     
     // To infinite run the controller, uncomment line below
     controller.SetFinishTime(0.0);
@@ -29,6 +33,12 @@ int XRF2_FRT_test::initialising()
     logger.initialise();
     // The FPGA has to be initialised at least once
     ico_io.init();
+
+    // Setting all output booleans to true
+    actuate_data.val1 = true;
+    actuate_data.val2 = true;
+    actuate_data.val3 = true;
+    actuate_data.val4 = true;
 
     return 1;
 }
@@ -58,9 +68,7 @@ int XRF2_FRT_test::run()
     data_to_be_logged.channel3 = sample_data.channel3;
     data_to_be_logged.channel4 = sample_data.channel4;
 
-    controller.Calculate(u, y);
-    if(controller.IsFinished())
-        return 1;
+    
 
     return 0;
 }
@@ -71,6 +79,17 @@ int XRF2_FRT_test::stopping()
     // Return 1 to go to stopped state
     logger.stop();                                // Stop logger
     evl_printf("Hello from stopping\n");          // Do something
+
+    // Halting all motors
+    actuate_data.val1 = true;
+    actuate_data.val2 = true;
+    actuate_data.val3 = true;
+    actuate_data.val4 = true;
+
+    actuate_data.pwm1 = 0;
+    actuate_data.pwm2 = 0;
+    actuate_data.pwm3 = 0;
+    actuate_data.pwm4 = 0;
 
     return 1;
 }
